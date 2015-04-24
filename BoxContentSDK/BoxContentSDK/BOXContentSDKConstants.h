418d40b583441c4abf7ae08a810926df10ab1517
Rico Yao <ryao@box.com>
2015-04-24 15:48:12 -0700

    Updated version to 1.0.2

--- BoxContentSDK/BoxContentSDK/BOXContentSDKConstants.h
+++ BoxContentSDK/BoxContentSDK/BOXContentSDKConstants.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 #define BOX_CONTENT_SDK_IDENTIFIER @"box-content-sdk"
-#define BOX_CONTENT_SDK_VERSION @"1.0.0"
+#define BOX_CONTENT_SDK_VERSION @"1.0.2"
 
 // API URLs
 extern NSString *const BOXAPIBaseURL;
