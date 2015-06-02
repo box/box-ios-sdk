244b7aefd0cb67690366a94a40324fdf76530fe6
Ben Lu <blu@box.com>
2015-06-02 11:46:53 -0700

    Add common super class called FileAccessRequest for FileDownloadRequest and private FileGetLinkRequest used in preview sdk

--- BoxContentSDK/BoxContentSDK/Requests/BOXFileDownloadRequest.h
+++ BoxContentSDK/BoxContentSDK/Requests/BOXFileDownloadRequest.h
@@ -3,19 +3,9 @@
 //  BoxContentSDK
 //
 
-#import "BOXRequestWithSharedLinkHeader.h"
+#import "BOXFileAccessRequest.h"
 
-@interface BOXFileDownloadRequest : BOXRequestWithSharedLinkHeader
-
-// This is not the etag of a particular version of the file, nor the sequential version number,
-// it is the ID of the version representation gotten from /files/<fileID>/versions
-@property (nonatomic, readwrite, strong) NSString *versionID;
-
-- (instancetype)initWithLocalDestination:(NSString *)destinationPath
-                                  fileID:(NSString *)fileID;
-
-- (instancetype)initWithOutputStream:(NSOutputStream *)outputStream
-                              fileID:(NSString *)fileID;
+@interface BOXFileDownloadRequest : BOXFileAccessRequest
 
 - (void)performRequestWithProgress:(BOXProgressBlock)progressBlock completion:(BOXErrorBlock)completionBlock;
 
--- BoxContentSDK/BoxContentSDK/Requests/BOXFileDownloadRequest.m
+++ BoxContentSDK/BoxContentSDK/Requests/BOXFileDownloadRequest.m
@@ -8,71 +8,20 @@
 
 #import "BOXAPIDataOperation.h"
 
-@interface BOXFileDownloadRequest ()
+
+@interface BOXFileAccessRequest ()
+
+@property (nonatomic, readonly, strong) NSOutputStream *outputStream;
 
 @property (nonatomic, readonly, strong) NSString *destinationPath;
-@property (nonatomic, readonly, strong) NSOutputStream *outputStream;
-@property (nonatomic, readonly, strong) NSString *fileID;
+
+@end
+
+@interface BOXFileDownloadRequest ()
 
 @end
 
 @implementation BOXFileDownloadRequest
-
-- (instancetype)initWithLocalDestination:(NSString *)destinationPath
-                                  fileID:(NSString *)fileID;
-{
-    if (self = [super init]) {
-        _destinationPath = destinationPath;
-        _fileID = fileID;
-    }
-    return self;
-}
-- (instancetype)initWithOutputStream:(NSOutputStream *)outputStream
-                              fileID:(NSString *)fileID
-{
-    if (self = [super init]) {
-        _outputStream = outputStream;
-        _fileID = fileID;
-    }
-    return self;
-}
-
-
-- (BOXAPIOperation *)createOperation
-{
-    NSURL *URL = [self URLWithResource:BOXAPIResourceFiles
-                                    ID:self.fileID
-                           subresource:BOXAPISubresourceContent
-                                 subID:nil];
-
-    NSDictionary *queryParameters = nil;
-
-    if (self.versionID.length > 0) {
-        queryParameters = @{BOXAPIParameterKeyFileVersion : self.versionID};
-    }
-
-    BOXAPIDataOperation *dataOperation = [self dataOperationWithURL:URL
-                                                         HTTPMethod:BOXAPIHTTPMethodGET
-                                              queryStringParameters:queryParameters
-                                                     bodyDictionary:nil
-                                                       successBlock:nil
-                                                       failureBlock:nil];
-
-    dataOperation.fileID = self.fileID;
-    
-    BOXAssert(self.outputStream != nil || self.destinationPath != nil, @"An output stream or destination file path must be specified.");
-    BOXAssert(!(self.outputStream != nil && self.destinationPath != nil), @"You cannot specify both an outputStream and a destination file path.");
-    
-    if (self.outputStream != nil) {
-        dataOperation.outputStream = self.outputStream;
-    } else {
-        dataOperation.outputStream = [[NSOutputStream alloc] initToFileAtPath:self.destinationPath append:NO];
-    }
-
-    [self addSharedLinkHeaderToRequest:dataOperation.APIRequest];
-
-    return dataOperation;
-}
 
 - (void)performRequestWithProgress:(BOXProgressBlock)progressBlock completion:(BOXErrorBlock)completionBlock
 {
@@ -101,18 +50,6 @@
     }
 
     [self performRequest];
-}
-
-#pragma mark - Superclass overidden methods
-
-- (NSString *)itemIDForSharedLink
-{
-    return self.fileID;
-}
-
-- (BOXAPIItemType *)itemTypeForSharedLink
-{
-    return BOXAPIItemTypeFile;
 }
 
 @end
