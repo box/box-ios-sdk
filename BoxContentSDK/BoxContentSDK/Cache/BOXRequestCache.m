99521a3cfd142941ef1d2ca2669a5f7938db9b68
Helen Kuo <helen@box.com>
2015-08-31 13:41:00 -0700

    Re-add sha1 calculation for cache keys and fix BoxFolderItemsRequestTests

--- BoxContentSDK/BoxContentSDK/Cache/BOXRequestCache.m
+++ BoxContentSDK/BoxContentSDK/Cache/BOXRequestCache.m
@@ -12,7 +12,6 @@
 #import "BOXRequest_Private.h"
 #import "BOXFolderItemsRequest.h"
 #import "PINCache.h"
-#import <CommonCrypto/CommonDigest.h>
 
 #define CACHE_FOLDER @"boxRequestCache"
 
@@ -152,30 +151,6 @@
         }
         self.cache = nil;
     }
-}
-
-#pragma mark - Private Helpers
-
--(NSString *)sha1:(NSString *)input
-{
-    if (!input) {
-        return nil;
-    }
-    
-    const char *cstr = [input cStringUsingEncoding:NSUTF8StringEncoding];
-    NSData *data = [NSData dataWithBytes:cstr length:input.length];
-    
-    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
-    
-    CC_SHA1(data.bytes, (CC_LONG)data.length, digest);
-    
-    NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH *2];
-    
-    for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++) {
-        [output appendFormat:@"%02x",digest[i]];
-    }
-    
-    return output;
 }
 
 @end
