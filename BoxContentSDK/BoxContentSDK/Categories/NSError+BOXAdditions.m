2b802e2acadb02ba96e76a82f4a7ee5bb55de274
Tom Carpel <tcarpel@box.com>
2015-09-02 12:00:16 -0700

    String updates from recent review

--- BoxContentSDK/BoxContentSDK/Categories/NSError+BOXAdditions.m
+++ BoxContentSDK/BoxContentSDK/Categories/NSError+BOXAdditions.m
@@ -17,7 +17,7 @@
     if ([self.domain isEqualToString:NSURLErrorDomain]) {
         switch (self.code) {
             case NSURLErrorNotConnectedToInternet:
-                result = NSLocalizedString(@"There was a problem connecting to Box. Please check your network connection.", @"Messsage: message shown in alert view when loading folders failed due to network connection");
+                result = NSLocalizedString(@"Unable to connect to Box. Please check your network connection and try again.", @"Messsage: message shown in alert view when loading folders failed due to network connection");
                 break;
             default:
                 result = NSLocalizedString(@"A network error occurred.", @"Narrative: Message explaining that a failure was caused by a network issue");
@@ -28,11 +28,11 @@
         switch (self.code) {
             case BOXContentSDKAPIErrorUnauthorized:
             case BOXContentSDKAPIErrorForbidden:
-                result = NSLocalizedString(@"You do not have the permissions to perform this action.", @"Narrative: Message explaining that a failure happened because the user does not have sufficient permissions or access required to perform the action");
+                result = NSLocalizedString(@"You do not have permission to perform this action.", @"Narrative: Message explaining that a failure happened because the user does not have sufficient permissions or access required to perform the action");
                 break;
                 
             case BOXContentSDKAPIErrorNotFound:
-                result = NSLocalizedString(@"The item does not exist.", @"Narrative: Message explaining that a failure happened because the item does not exist");
+                result = NSLocalizedString(@"This item does not exist.", @"Narrative: Message explaining that a failure happened because the item does not exist");
                 break;
                 
             case BOXContentSDKAPIErrorConflict:
@@ -45,7 +45,7 @@
     }
     
     if (result == nil) {
-        result = NSLocalizedString(@"Sorry, we could not complete the requested action.", @"Narrative: Message explaining that a failure happened.");
+        result = NSLocalizedString(@"Unable to complete the requested action.", @"Narrative: Message explaining that a failure happened.");
     }
     
     return result;
