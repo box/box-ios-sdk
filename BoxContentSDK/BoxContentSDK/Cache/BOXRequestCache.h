03ffec6e7e0402726c9d4994d2decf89f3d0aa78
Helen Kuo <helen@box.com>
2015-09-07 18:08:42 -0700

    Add check for errors which should result in cached response removal.

--- BoxContentSDK/BoxContentSDK/Cache/BOXRequestCache.h
+++ BoxContentSDK/BoxContentSDK/Cache/BOXRequestCache.h
@@ -14,11 +14,46 @@
 
 @property (nonatomic, readonly, copy) NSString *userID;
 
+/**
+ *  Creates a BOXRequestCache for a given user ID.
+ *
+ *  @param userID id of the user for the cache.
+ *
+ *  @return BOXRequestCache for the user.
+ */
 - (instancetype)initWithUserID:(NSString *)userID;
 
+/**
+ *  Performs the cacheBlock with the value retrieved for the specified key.
+ *
+ *  @param key        key associated with the request to retrieve.
+ *  @param cacheBlock block to be executed when the cached request has been retrieved.
+ */
 - (void)fetchCacheForKey:(NSString *)key cacheBlock:(void(^)(NSDictionary *dictionary))cacheBlock;
+
+/**
+ *  Removes the cached response for the specified key.
+ *
+ *  @param key key for the request to remove from the cache.
+ */
 - (void)removeCacheForKey:(NSString *)key;
+
+/**
+ *  Updates the cached response for a key.
+ *
+ *  @param key            key associated with the response.
+ *  @param JSONDictionary JSON dictionary for the response.
+ */
 - (void)updateCacheForKey:(NSString *)key withResponse:(NSDictionary *)JSONDictionary;
+
+/**
+ *  Removes all responses currently stored in the cache.
+ */
 - (void)removeAllCachedResponses;
+
+/**
+ *  Clears the cache for logout.
+ */
 - (void)clearForLogout;
+
 @end
