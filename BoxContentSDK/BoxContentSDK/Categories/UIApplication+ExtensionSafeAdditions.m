963e47dc9ec6683661c1b47c41d84e230ccf7b76
Clement Rousselle <crousselle@mbp-crousselle.local>
2016-06-14 09:54:42 -0700

    Fix the UIApplication category logic that was reversed.

--- BoxContentSDK/BoxContentSDK/Categories/UIApplication+ExtensionSafeAdditions.m
+++ BoxContentSDK/BoxContentSDK/Categories/UIApplication+ExtensionSafeAdditions.m
@@ -11,34 +11,44 @@
 + (UIApplication *)box_sharedApplication
 {
 #ifndef BOX_TARGET_IS_EXTENSION
-    // If we are compiling from a non-extension target, use the regular sharedApplication.
-    return [UIApplication sharedApplication];
-#else
     // If we are compiling from an extension, return nil here since UIApplication isn't extension-safe.
     return nil;
+#else
+    // If we are compiling from a non-extension target, use the regular sharedApplication.
+    return [UIApplication sharedApplication];
 #endif
 }
 
 - (BOOL)box_canOpenURL:(NSURL *)url
 {
 #ifndef BOX_TARGET_IS_EXTENSION
-    // If we are compiling from a non-extension target, use the regular sharedApplication.
-    return [[UIApplication sharedApplication] canOpenURL:url];
-#else
     // If we are compiling from an extension, do nothing and return NO here since UIApplication isn't extension-safe.
     return NO;
+#else
+    // If we are compiling from a non-extension target, use the regular sharedApplication.
+    return [[UIApplication sharedApplication] canOpenURL:url];
 #endif
 }
 
 - (BOOL)box_openURL:(NSURL*)url
 {
 #ifndef BOX_TARGET_IS_EXTENSION
-    // If we are compiling from a non-extension target, use the regular sharedApplication.
-    return [[UIApplication sharedApplication] openURL:url];
-#else
     // If we are compiling from an extension, do nothing and return NO here since UIApplication isn't extension-safe.
     return NO;
+#else
+    // If we are compiling from a non-extension target, use the regular sharedApplication.
+    return [[UIApplication sharedApplication] openURL:url];
 #endif
 }
 
+- (UIWindow *)box_window
+{
+#ifndef BOX_TARGET_IS_EXTENSION
+    // If we are compiling from an extension, do nothing and return NO here since UIApplication isn't extension-safe.
+    return nil;
+#else
+    // If we are compiling from a non-extension target, use the regular sharedApplication.
+    return [[[UIApplication sharedApplication] delegate] window];
+#endif
+}
 @end
