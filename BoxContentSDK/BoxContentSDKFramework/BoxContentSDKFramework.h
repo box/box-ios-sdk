554181ca033b79e19fde6784336ccfe8c1a400f6
Scott Liu <sliu@mbp-002013.ad.whirl.net>
2017-02-08 16:14:31 -0800

    First step in changing BoxContentSDK into a framework.

---
+++ BoxContentSDK/BoxContentSDKFramework/BoxContentSDKFramework.h
@@ -0,0 +1,19 @@
+//
+//  BoxContentSDKFramework.h
+//  BoxContentSDKFramework
+//
+//  Created by Scott Liu on 2/8/17.
+//  Copyright © 2017 Box. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BoxContentSDKFramework.
+FOUNDATION_EXPORT double BoxContentSDKFrameworkVersionNumber;
+
+//! Project version string for BoxContentSDKFramework.
+FOUNDATION_EXPORT const unsigned char BoxContentSDKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoxContentSDKFramework/PublicHeader.h>
+
+
