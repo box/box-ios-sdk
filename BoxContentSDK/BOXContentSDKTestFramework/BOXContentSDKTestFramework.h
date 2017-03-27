fd877919f808ce2b723dabbde16418a5ba9d5cb8
Clement Rousselle <clement@box.com>
2017-03-27 15:18:19 -0700

    Create a BOXContentSDKTestFramework.

---
+++ BoxContentSDK/BOXContentSDKTestFramework/BOXContentSDKTestFramework.h
@@ -0,0 +1,14 @@
+//
+//  BOXContentSDKTestFramework.h
+//  BOXContentSDKTestFramework
+//
+//  Created by Clement Rousselle on 3/27/17.
+//  Copyright © 2017 Box. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BOXRequestTestCase.h"
+#import "BOXCannedURLProtocol.h"
+#import "BOXContentSDKTestCase.h"
+#import "BOXCannedResponse.h"
