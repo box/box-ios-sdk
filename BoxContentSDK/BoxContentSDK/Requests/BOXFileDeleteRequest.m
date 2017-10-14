576ed6af5a19fef098c053e2a371cf2dbe9e71d2
jdizoglio <jdizoglio@box.com>
2017-10-13 17:54:09 -0700

    Adding background tasks to delete and trash file requests

--- BoxContentSDK/BoxContentSDK/Requests/BOXFileDeleteRequest.m
+++ BoxContentSDK/BoxContentSDK/Requests/BOXFileDeleteRequest.m
@@ -15,6 +15,15 @@
 
 @property (nonatomic, readwrite, strong) NSString *fileID;
 
+/// Properties related to Background tasks
+
+/**
+ Check if the request can executre on background. Requires valid associateId and requestDirectoryPath
+ 
+ @return BOOL Yes for can preform on background
+ */
+- (BOOL)shouldPerformBackgroundOperation;
+
 @end
 
 @implementation BOXFileDeleteRequest
@@ -34,6 +43,18 @@
     return self;
 }
 
+- (instancetype)initWithFileID:(NSString *)fileID
+                     isTrashed:(BOOL)isTrashed
+                   associateId:(NSString *)associateId
+{
+    if (self = [super init]) {
+        _fileID = fileID;
+        _isTrashed = isTrashed;
+        _associateId = associateId;
+    }
+    return self;
+}
+
 - (BOXAPIOperation *)createOperation
 {
     NSString *subresource = nil;
@@ -42,52 +63,107 @@
         subresource = BOXAPISubresourceTrash;
     }
     
-    NSURL *URL = [self URLWithResource:BOXAPIResourceFiles
+    NSURL *url = [self URLWithResource:BOXAPIResourceFiles
                                     ID:self.fileID
                            subresource:subresource
                                  subID:nil];
-
-    BOXAPIJSONOperation *JSONoperation = [self JSONOperationWithURL:URL
-                                                         HTTPMethod:BOXAPIHTTPMethodDELETE
-                                              queryStringParameters:nil
-                                                     bodyDictionary:nil
-                                                   JSONSuccessBlock:nil
-                                                       failureBlock:nil];
-    if ([self.matchingEtag length] > 0) {
-        [JSONoperation.APIRequest setValue:self.matchingEtag forHTTPHeaderField:BOXAPIHTTPHeaderIfMatch];
+    
+    if ([self shouldPerformBackgroundOperation] == YES) {
+        BOXAPIDataOperation *dataOperation = [self dataOperationWithURL:url
+                                                             HTTPMethod:BOXAPIHTTPMethodDELETE
+                                                  queryStringParameters:nil
+                                                         bodyDictionary:nil
+                                                           successBlock:nil
+                                                           failureBlock:nil
+                                                            associateId:self.associateId];
+        
+        NSString *requestDirectory = self.requestDirectoryPath;
+        NSString *destinationPath = [requestDirectory stringByAppendingPathComponent:self.associateId];
+        dataOperation.destinationPath = destinationPath;
+        
+        return dataOperation;
+    } else {
+        BOXAPIJSONOperation *JSONoperation = [self JSONOperationWithURL:url
+                                                             HTTPMethod:BOXAPIHTTPMethodDELETE
+                                                  queryStringParameters:nil
+                                                         bodyDictionary:nil
+                                                       JSONSuccessBlock:nil
+                                                           failureBlock:nil];
+        
+        if ([self.matchingEtag length] > 0) {
+            [JSONoperation.APIRequest setValue:self.matchingEtag forHTTPHeaderField:BOXAPIHTTPHeaderIfMatch];
+        }
+        
+        return JSONoperation;
     }
-
-    return JSONoperation;
 }
 
 - (void)performRequestWithCompletion:(BOXErrorBlock)completionBlock
 {
-    BOOL isMainThread = [NSThread isMainThread];
-    BOXAPIJSONOperation *folderOperation = (BOXAPIJSONOperation *)self.operation;
-    
-    folderOperation.success = ^(NSURLRequest *request, NSHTTPURLResponse *response, NSDictionary *JSONDictionary) {
-
-        if ([self.cacheClient respondsToSelector:@selector(cacheFileDeleteRequest:error:)]) {
-            [self.cacheClient cacheFileDeleteRequest:self error:nil];
-        }
-        if (completionBlock) {
-            [BOXDispatchHelper callCompletionBlock:^{
-                completionBlock(nil);
-            } onMainThread:isMainThread];
-        }
-    };
-    folderOperation.failure = ^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, NSDictionary *JSONDictionary) {
-
-        if ([self.cacheClient respondsToSelector:@selector(cacheFileDeleteRequest:error:)]) {
-            [self.cacheClient cacheFileDeleteRequest:self error:error];
-        }
-        if (completionBlock) {
-            [BOXDispatchHelper callCompletionBlock:^{
-                completionBlock(error);
-            } onMainThread:isMainThread];
-        }
-    };
-    [self performRequest];
+    if ([self shouldPerformBackgroundOperation] == YES) {
+        BOOL isMainThread = [NSThread isMainThread];
+        
+        BOXAPIDataOperation *fileOperation = (BOXAPIDataOperation *)self.operation;
+        __weak BOXAPIDataOperation *weakFileOperation = fileOperation;
+        
+        fileOperation.successBlock = ^(NSString *modelID, long long expectedTotalBytes) {
+            if ([self.cacheClient respondsToSelector:@selector(cacheFileDeleteRequest:error:)]) {
+                [self.cacheClient cacheFileDeleteRequest:self error:nil];
+            }
+            
+            if ([[NSFileManager defaultManager] fileExistsAtPath: weakFileOperation.destinationPath]) {
+                NSError *error = nil;
+                [[NSFileManager defaultManager] removeItemAtPath:weakFileOperation.destinationPath error:&error];
+            }
+            
+            if (completionBlock) {
+                [BOXDispatchHelper callCompletionBlock:^{
+                    completionBlock(nil);
+                } onMainThread:isMainThread];
+            }
+        };
+        
+        fileOperation.failureBlock = ^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
+            if ([self.cacheClient respondsToSelector:@selector(cacheFileDeleteRequest:error:)]) {
+                [self.cacheClient cacheFileDeleteRequest:self error:error];
+            }
+            if (completionBlock) {
+                [BOXDispatchHelper callCompletionBlock:^{
+                    completionBlock(error);
+                } onMainThread:isMainThread];
+            }
+        };
+        
+        [self performRequest];
+    } else {
+        
+        BOOL isMainThread = [NSThread isMainThread];
+        BOXAPIJSONOperation *folderOperation = (BOXAPIJSONOperation *)self.operation;
+        
+        folderOperation.success = ^(NSURLRequest *request, NSHTTPURLResponse *response, NSDictionary *JSONDictionary) {
+            
+            if ([self.cacheClient respondsToSelector:@selector(cacheFileDeleteRequest:error:)]) {
+                [self.cacheClient cacheFileDeleteRequest:self error:nil];
+            }
+            if (completionBlock) {
+                [BOXDispatchHelper callCompletionBlock:^{
+                    completionBlock(nil);
+                } onMainThread:isMainThread];
+            }
+        };
+        folderOperation.failure = ^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, NSDictionary *JSONDictionary) {
+            
+            if ([self.cacheClient respondsToSelector:@selector(cacheFileDeleteRequest:error:)]) {
+                [self.cacheClient cacheFileDeleteRequest:self error:error];
+            }
+            if (completionBlock) {
+                [BOXDispatchHelper callCompletionBlock:^{
+                    completionBlock(error);
+                } onMainThread:isMainThread];
+            }
+        };
+        [self performRequest];
+    }
 }
 
 #pragma mark - Superclass overidden methods
@@ -100,6 +176,13 @@
 - (BOXAPIItemType *)itemTypeForSharedLink
 {
     return BOXAPIItemTypeFile;
+}
+
+#pragma mark - Private Helper methods
+
+- (BOOL)shouldPerformBackgroundOperation
+{
+    return (self.associateId.length > 0 && self.requestDirectoryPath.length > 0);
 }
 
 @end
