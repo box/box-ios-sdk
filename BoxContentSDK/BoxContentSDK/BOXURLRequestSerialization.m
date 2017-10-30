856b944eceb35b8149cb1535c63a29451cbe57c7
Thuy Nguyen <thuy@box.com>
2017-10-30 16:41:03 -0700

    IOS-14788 (p2): remove unused schedule on runloop when serializing multipart request

--- BoxContentSDK/BoxContentSDK/BOXURLRequestSerialization.m
+++ BoxContentSDK/BoxContentSDK/BOXURLRequestSerialization.m
@@ -441,9 +441,6 @@ forHTTPHeaderField:(NSString *)field
     __block NSError *error = nil;
 
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        [inputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-        [outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-
         [inputStream open];
         [outputStream open];
 
@@ -1215,7 +1212,6 @@ typedef enum {
             _phase = BOXHeaderPhase;
             break;
         case BOXHeaderPhase:
-            [self.inputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSRunLoopCommonModes];
             [self.inputStream open];
             _phase = BOXBodyPhase;
             break;
