a53799741c3cfe49f872bea7b4954e5da52c6da3
Matt Kiazyk <matt.kiazyk@robotsandpencils.com>
2018-04-06 12:24:01 -0500

    Fix BOXContentSDK Tests. Added TestApp to fix keychain tests.
    Add test host

---
+++ BoxContentSDK/TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Matthew Kiazyk on 2018-04-06.
+//  Copyright © 2018 Box. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BoxContentSDK/TestApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestApp
+//
+//  Created by Matthew Kiazyk on 2018-04-06.
+//  Copyright © 2018 Box. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
