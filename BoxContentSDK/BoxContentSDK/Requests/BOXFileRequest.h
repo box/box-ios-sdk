8e080f654698a2f1ae1df189cc75a922ede7a68a
Prithvi Jutur <pjutur@box.com>
2018-04-24 11:07:21 -0700

    remote DICOM reps

--- BoxContentSDK/BoxContentSDK/Requests/BOXFileRequest.h
+++ BoxContentSDK/BoxContentSDK/Requests/BOXFileRequest.h
@@ -18,7 +18,6 @@
  - BOXRepresentationRequestJPGRepresentation:               Request JPG representions if available for the given file
  - BOXRepresentationRequestMP3Representation:               Request PNG representions if available for the given file
  - BOXRepresentationRequestMP4Representation:               Request MP4 representions if available for the given file
- - BOXRepresentationRequestDicomRepresentation:             Request DICOM representions available for DICOM file
  - BOXRepresentationRequesteExtractedTextRepresentation:    Request extracted text if unformatted text is contained in a document (non-image)
  */
 typedef NS_OPTIONS(NSUInteger, BOXRepresentationRequestOptions) {
@@ -30,8 +29,7 @@ typedef NS_OPTIONS(NSUInteger, BOXRepresentationRequestOptions) {
     BOXRepresentationRequestJPGRepresentation                  = 1 << 5,
     BOXRepresentationRequestMP3Representation                  = 1 << 6,
     BOXRepresentationRequestMP4Representation                  = 1 << 7,
-    BOXRepresentationRequestDICOMRepresentation                = 1 << 8,
-    BOXRepresentationRequesteExtractedTextRepresentation       = 1 << 9
+    BOXRepresentationRequesteExtractedTextRepresentation       = 1 << 8
 } NS_ENUM_AVAILABLE_IOS(10_0);
 
 @interface BOXFileRequest : BOXRequestWithSharedLinkHeader
