1694f9039faf069e3714ed9fb29d27a6d5abad44
Chad Burnette <chadburnette@me.com>
2018-08-03 17:48:57 -0400

    adding support for creating a BoxContentClient wrapper that uses server auth to retrieve tokens and can be instantiated with only a token and a user id; supports app users, token downscoping, service accounts, etc.

---
+++ BoxContentSDK/BoxContentSDK/Clients/BoxContentClientServerAuthWrapper.m
@@ -0,0 +1,35 @@
+#import "BoxContentClientServerAuthWrapper.h"
+#import "BoxAbstractSession.h"
+
+@interface BoxContentClientServerAuthWrapper ()
+
+@property (nonatomic, readonly, strong) void (^fetchTokenBlock) (void (^completion)(NSString *, NSDate *, NSError *), NSString *userId);
+
+@end
+
+@implementation BoxContentClientServerAuthWrapper
+
+- (instancetype)initWithInitialToken:(NSString *)token
+                           forUserId:(NSString *)userId
+                 withFetchTokenBlock:(void (^) (void (^)(NSString *, NSDate *, NSError *), NSString *userId))fetchTokenBlock
+{
+    if (self = [super init]) {
+        _userId = userId;
+        _fetchTokenBlock = fetchTokenBlock;
+        
+        _boxClient = [BOXContentClient clientForNewSession];
+        [_boxClient setAccessTokenDelegate:self];
+        
+        BOXAbstractSession *session = [_boxClient session];
+        session.accessToken = token;
+    }
+    
+    return self;
+}
+
+- (void)fetchAccessTokenWithCompletion:(void (^)(NSString *, NSDate *, NSError *))completion
+{
+    _fetchTokenBlock(completion, _userId);
+}
+
+@end
