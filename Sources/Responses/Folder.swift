3ddd495df440f192506c5e74120a44a8249baffd
Sujay Garlanka <sujay.garlanka@gmail.com>
2020-02-25 23:13:58 -0800

    Update Folder.swift

--- Sources/Responses/Folder.swift
+++ Sources/Responses/Folder.swift
@@ -76,7 +76,7 @@ public class Folder: BoxModel {
     public let id: String
     /// A unique ID for use with the events.
     public let sequenceId: String?
-    /// The entity tag of this web link. Used with If-Match headers.
+    /// The entity tag of this folder. Used with If-Match headers.
     public let etag: String?
     /// The name of the folder.
     public let name: String?
