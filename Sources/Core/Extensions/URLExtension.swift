24231f9ebc2c4fc40c541d8a038637b57d23d364
Sujay Garlanka <sujay.garlanka@gmail.com>
2020-05-12 11:52:41 -0400

    add path sanitization

--- Sources/Core/Extensions/URLExtension.swift
+++ Sources/Core/Extensions/URLExtension.swift
@@ -19,6 +19,20 @@ extension URL {
     }
 
     private static func make(from string: String, relativeTo baseURL: URL) -> URL {
+        // This check is there to only run sanitization when string is path and not a full custom URL. Full URLs are just returned
+        if URL(string: string)?.scheme == nil {
+            let validParams = ["2.0", "thumbnail.jpg", "thumbnail.png"]
+            var sanitizedURL = string
+            validParams.forEach { param in
+                sanitizedURL = sanitizedURL.replacingOccurrences(of: param, with: "")
+            }
+            let range = NSRange(location: 0, length: sanitizedURL.utf16.count)
+            // Checks that url paths with relative paths like /../ or other invalid path parameters are not sent to the API
+            let regex = try? NSRegularExpression(pattern: "^[a-zA-Z0-9!@#$%^&*()_+\\/-]*$")
+            if regex?.firstMatch(in: sanitizedURL, options: [], range: range) == nil {
+                fatalError("An invalid path parameter exists in \(string). All parameters must be alphanumeric.")
+            }
+        }
         guard let url = URL(string: string, relativeTo: baseURL) else {
             fatalError("Could not create URL from \(string.isEmpty ? "empty string" : string) relative to base URL: \(baseURL)")
         }
--- Tests/Requests/URLs/URLExtensionTest.swift
+++ Tests/Requests/URLs/URLExtensionTest.swift
@@ -17,6 +17,7 @@ class URLExtensionTest: QuickSpec {
 
         let configuration: BoxSDKConfiguration = try! BoxSDKConfiguration(clientId: "", clientSecret: "")
         let customURLPart: String = "/testurl"
+        let invalidURLPart: String = "/test/../"
 
         describe("boxAPIEndpoint()") {
             it("should create an API Endpoint URL containing base url from configuration") {
@@ -24,13 +25,21 @@ class URLExtensionTest: QuickSpec {
                 expect(url.absoluteString).to(equal(configuration.apiBaseURL.absoluteString.appending(customURLPart)))
                 expect(url.absoluteString).to(contain(configuration.apiBaseURL.absoluteString))
             }
+
+            it("should fail to create an API Endpoint URL") {
+                expect { _ = URL.boxAPIEndpoint(invalidURLPart, configuration: configuration) }.to(throwAssertion())
+            }
         }
 
-        describe("boxAPIEndpoint()") {
+        describe("boxUploadEndpoint()") {
             it("should create an Upload Endpoint URL containing base url from configuration") {
                 let url = URL.boxUploadEndpoint(customURLPart, configuration: configuration)
                 expect(url.absoluteString).to(equal(configuration.uploadApiBaseURL.absoluteString.appending(customURLPart)))
                 expect(url.absoluteString).to(contain(configuration.uploadApiBaseURL.absoluteString))
+            }
+
+            it("should fail to create an Upload Endpoint URL") {
+                expect { _ = URL.boxUploadEndpoint(invalidURLPart, configuration: configuration) }.to(throwAssertion())
             }
 
             context("when providing full url string") {
