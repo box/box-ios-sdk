edb480cc09ed288b20b43fec1897cb8801bdd2f3
Artur Jankowski <ajankowski@box.com>
2021-09-22 11:37:28 +0200

    Fix tests in CollaborationItem

--- Tests/Responses/CollaborationItemSpecs.swift
+++ Tests/Responses/CollaborationItemSpecs.swift
@@ -18,7 +18,7 @@ class CollaborationItemSpecs: QuickSpec {
             describe("init()") {
 
                 it("should correctly deserialize a file from full JSON representation") {
-                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullCollaborationItemFile", ofType: "json") else {
+                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullFile", ofType: "json") else {
                         fail("Could not find fixture file.")
                         return
                     }
@@ -34,35 +34,14 @@ class CollaborationItemSpecs: QuickSpec {
                         }
 
                         expect(file.type).to(equal("file"))
-                        expect(file.id).to(equal("12345"))
-                        expect(file.etag).to(equal("0"))
-                        expect(file.sequenceId).to(equal("0"))
-                        expect(file.sha1).to(equal("85136C79CBF9FE36BB9D05D0639C70C265C18D37"))
-                        expect(file.name).to(equal("File1.pdf"))
-                        expect(file.description).to(equal("Test description"))
-                        expect(file.size).to(equal(629_644))
-
-                        guard let pathCollection = file.pathCollection else {
-                            fail("Expected path collection to be present")
-                            return
-                        }
-                        expect(pathCollection.totalCount).to(equal(1))
-                        expect(pathCollection.entries?.count).to(equal(1))
-                        expect(pathCollection.entries?[0]).toNot(beNil())
-                        expect(pathCollection.entries?[0].type).to(equal("folder"))
-                        expect(pathCollection.entries?[0].id).to(equal("12345"))
-                        expect(pathCollection.entries?[0].sequenceId).to(equal("3"))
-                        expect(pathCollection.entries?[0].etag).to(equal("1"))
-                        expect(pathCollection.entries?[0].name).to(equal("All Files"))
-                        expect(file.createdAt?.iso8601).to(equal("2012-12-12T18:53:43Z"))
-                        expect(file.modifiedAt?.iso8601).to(equal("2012-12-12T18:53:43Z"))
-                        expect(file.contentCreatedAt?.iso8601).to(equal("2012-12-12T18:53:43Z"))
-                        expect(file.contentModifiedAt?.iso8601).to(equal("2012-12-12T18:53:43Z"))
-                        expect(file.parent?.type).to(equal("folder"))
-                        expect(file.parent?.id).to(equal("12345"))
-                        expect(file.parent?.sequenceId).to(equal("3"))
-                        expect(file.parent?.etag).to(equal("1"))
-                        expect(file.parent?.name).to(equal("Test Folder"))
+                        expect(file.id).to(equal("11111"))
+                        expect(file.etag).to(equal("2"))
+                        expect(file.sequenceId).to(equal("2"))
+                        expect(file.sha1).to(equal("14acc506f2021b2f1a9d81097ca82e14887b34fe"))
+                        expect(file.name).to(equal("script.js"))
+                        expect(file.extension).to(equal("js"))
+                        expect(file.description).to(equal("My test script"))
+                        expect(file.size).to(equal(33510))
                     }
                     catch {
                         fail("Failed with Error: \(error)")
@@ -70,7 +49,7 @@ class CollaborationItemSpecs: QuickSpec {
                 }
 
                 it("should correctly deserialize a folder from full JSON representation") {
-                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullCollaborationItemFolder", ofType: "json") else {
+                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullFolder", ofType: "json") else {
                         fail("Could not find fixture file.")
                         return
                     }
@@ -95,28 +74,14 @@ class CollaborationItemSpecs: QuickSpec {
                         expect(folder.description).to(equal("A folder for testing"))
                         expect(folder.isCollaborationRestrictedToEnterprise).to(beTrue())
                         expect(folder.size).to(equal(814_688_861))
-                        expect(folder.pathCollection?.totalCount).to(equal(1))
-                        expect(folder.pathCollection?.entries?.count).to(equal(1))
-                        expect(folder.pathCollection?.entries?[0].type).to(equal("folder"))
-                        expect(folder.pathCollection?.entries?[0].id).to(equal("0"))
-                        expect(folder.pathCollection?.entries?[0].sequenceId).to(beNil())
-                        expect(folder.pathCollection?.entries?[0].etag).to(beNil())
-                        expect(folder.pathCollection?.entries?[0].name).to(equal("All Files"))
-                        expect(folder.folderUploadEmail?.access).to(equal("collaborators"))
-                        expect(folder.folderUploadEmail?.email).to(equal("Test_Fo.sidu576dbfn@u.box.com"))
-                        expect(folder.parent?.type).to(equal("folder"))
-                        expect(folder.parent?.id).to(equal("0"))
-                        expect(folder.parent?.sequenceId).to(beNil())
-                        expect(folder.parent?.etag).to(beNil())
-                        expect(folder.parent?.name).to(equal("All Files"))
                     }
                     catch {
                         fail("Failed with Error: \(error)")
                     }
                 }
 
-                it("should throw BoxCodingError.valueMismatch exception when deserialize an object with an unknown value in type filed") {
-                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullCollaborationItem_ValueFormatMismatch", ofType: "json") else {
+                it("should throw BoxCodingError.valueMismatch exception when deserialize an object with an unknown value in type field") {
+                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullFile_ValueFormatMismatch", ofType: "json") else {
                         fail("Could not find fixture file.")
                         return
                     }
@@ -133,7 +98,7 @@ class CollaborationItemSpecs: QuickSpec {
                 }
 
                 it("should throw BoxCodingError.typeMismatch exception when deserialize object with no type field") {
-                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullCollaborationItem_MissingRequiredField", ofType: "json") else {
+                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullFile_MissingRequiredField", ofType: "json") else {
                         fail("Could not find fixture file.")
                         return
                     }
@@ -152,7 +117,7 @@ class CollaborationItemSpecs: QuickSpec {
 
             describe("rawData") {
                 it("should be equal to json data used to create the CollaborationItem file type object") {
-                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullCollaborationItemFile", ofType: "json") else {
+                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullFile", ofType: "json") else {
                         fail("Could not find fixture file.")
                         return
                     }
@@ -171,7 +136,7 @@ class CollaborationItemSpecs: QuickSpec {
                 }
 
                 it("should be equal to json data used to create the CollaborationItem folder type object") {
-                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullCollaborationItemFolder", ofType: "json") else {
+                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullFolder", ofType: "json") else {
                         fail("Could not find fixture file.")
                         return
                     }
@@ -192,7 +157,7 @@ class CollaborationItemSpecs: QuickSpec {
 
             describe("debugDescription") {
                 it("should return correct description for a file type") {
-                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullCollaborationItemFile", ofType: "json") else {
+                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullFile", ofType: "json") else {
                         fail("Could not find fixture file.")
                         return
                     }
@@ -201,7 +166,7 @@ class CollaborationItemSpecs: QuickSpec {
                         let contents = try String(contentsOfFile: filepath)
                         let jsonDict = try JSONSerialization.jsonObject(with: contents.data(using: .utf8)!) as! [String: Any]
                         let collaborationItem = try CollaborationItem(json: jsonDict)
-                        expect(collaborationItem.debugDescription).to(equal("file Optional(\"File1.pdf\")"))
+                        expect(collaborationItem.debugDescription).to(equal("file Optional(\"script.js\")"))
                     }
                     catch {
                         fail("Failed with Error: \(error)")
@@ -209,7 +174,7 @@ class CollaborationItemSpecs: QuickSpec {
                 }
 
                 it("should return correct description for folder type") {
-                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullCollaborationItemFolder", ofType: "json") else {
+                    guard let filepath = Bundle(for: type(of: self)).path(forResource: "FullFolder", ofType: "json") else {
                         fail("Could not find fixture file.")
                         return
                     }
