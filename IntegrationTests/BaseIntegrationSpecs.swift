832a8dba0898c08e047a625c5bff3fc31b29b283
Artur Jankowski <ajankowski@box.com>
2021-12-06 15:21:55 +0100

    chore: Add integration tests for folder module (#803)

---
+++ IntegrationTests/BaseIntegrationSpecs.swift
@@ -0,0 +1,54 @@
+//
+//  BaseIntegrationSpecs.swift
+//  BoxSDKIntegrationTests-iOS
+//
+//  Created by Artur Jankowski on 01/12/2021.
+//  Copyright © 2021 box. All rights reserved.
+//
+
+@testable import BoxSDK
+import Nimble
+import Quick
+
+class BaseIntegrationSpecs: QuickSpec {
+    var client: BoxClient!
+
+    override func spec() {
+        beforeSuite {
+            self.client = BoxSDK.getClient(token: Configuration.developerToken)
+        }
+    }
+
+    // MARK: Section with helper methods used in tests
+
+    func createFolder(name: String, parentId: String = "0", callback: @escaping (Folder) -> Void) {
+        waitUntil(timeout: .seconds(Constants.defaultTimeout)) { done in
+            self.client.folders.create(name: name, parentId: parentId) { result in
+                switch result {
+                case let .success(folder):
+                    callback(folder)
+                case let .failure(error):
+                    fail("Expected create call to suceeded, but instead got \(error)")
+                }
+
+                done()
+            }
+        }
+    }
+
+    func deleteFolder(_ folder: Folder?, recursive: Bool = false) {
+        guard let folder = folder else {
+            return
+        }
+
+        waitUntil(timeout: .seconds(Constants.defaultTimeout)) { done in
+            self.client.folders.delete(folderId: folder.id, recursive: recursive) { result in
+                if case let .failure(error) = result {
+                    fail("Expected delete call to succeed, but instead got \(error)")
+                }
+
+                done()
+            }
+        }
+    }
+}
---
+++ IntegrationTests/Configuration.swift
@@ -0,0 +1,13 @@
+//
+//  Configuration.swift
+//  BoxSDKIntegrationTests-iOS
+//
+//  Created by Artur Jankowski on 01/12/2021.
+//  Copyright © 2021 box. All rights reserved.
+//
+
+import Foundation
+
+enum Configuration {
+    static var developerToken: String = "" // Please put your DEVELOPER TOKEN here
+}
