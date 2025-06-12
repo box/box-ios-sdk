a7233d756a679007958e08c9555aed871a6ba4eb
Artur Jankowski <ajankowski@box.com>
2025-06-12 11:46:06 +0200

    feat!: Bump minimum supported platform versions (#936)

--- Package.swift
+++ Package.swift
@@ -12,7 +12,10 @@ import PackageDescription
 let package = Package(
     name: "BoxSDKSuite",
     platforms: [
-        .iOS(.v11)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
         .library(
