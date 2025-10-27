6ed575c3a0023bce2abcb1d883d51b5a86f05ce2
box-sdk-build <sdk-build@box.com>
2025-10-27 11:50:29 -0700

    fix: remove enum types from GET enterprise configurations endpoint (box/box-openapi#560)

--- BoxSdkGen/Sources/Managers/EnterpriseConfigurations/GetEnterpriseConfigurationByIdV2025R0QueryParams.swift
+++ BoxSdkGen/Sources/Managers/EnterpriseConfigurations/GetEnterpriseConfigurationByIdV2025R0QueryParams.swift
@@ -1,16 +1,16 @@
 import Foundation
 
 public class GetEnterpriseConfigurationByIdV2025R0QueryParams {
-    /// The comma-delimited list of the enterprise configuration categories. 
+    /// A comma-separated list of the enterprise configuration categories.
     /// Allowed values: `security`, `content_and_sharing`, `user_settings`, `shield`.
-    public let categories: String
+    public let categories: [String]
 
     /// Initializer for a GetEnterpriseConfigurationByIdV2025R0QueryParams.
     ///
     /// - Parameters:
-    ///   - categories: The comma-delimited list of the enterprise configuration categories. 
+    ///   - categories: A comma-separated list of the enterprise configuration categories.
     ///     Allowed values: `security`, `content_and_sharing`, `user_settings`, `shield`.
-    public init(categories: String) {
+    public init(categories: [String]) {
         self.categories = categories
     }
 
