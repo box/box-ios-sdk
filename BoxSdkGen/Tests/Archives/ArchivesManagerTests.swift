0f92de081402f37abe8615c3775c0a2316a68a53
box-sdk-build <sdk-build@box.com>
2025-11-07 04:55:49 -0800

    test: Update archive tests (box/box-codegen#865)

--- BoxSdkGen/Tests/Archives/ArchivesManagerTests.swift
+++ BoxSdkGen/Tests/Archives/ArchivesManagerTests.swift
@@ -14,9 +14,16 @@ class ArchivesManagerTests: RetryableTestCase {
     public func testArchivesCreateListDelete() async throws {
         await runWithRetryAsync {
             let archiveName: String = Utils.getUUID()
-            let archive: ArchiveV2025R0 = try await client.archives.createArchiveV2025R0(requestBody: CreateArchiveV2025R0RequestBody(name: archiveName))
+            let archiveDescription: String = "Test Archive Description"
+            let archive: ArchiveV2025R0 = try await client.archives.createArchiveV2025R0(requestBody: CreateArchiveV2025R0RequestBody(name: archiveName, description: archiveDescription))
             XCTAssertTrue(Utils.Strings.toString(value: archive.type) == "archive")
             XCTAssertTrue(archive.name == archiveName)
+            XCTAssertTrue(archive.description == archiveDescription)
+            let newArchiveName: String = Utils.getUUID()
+            let newArchiveDescription: String = "Updated Archive Description"
+            let updatedArchive: ArchiveV2025R0 = try await client.archives.updateArchiveByIdV2025R0(archiveId: archive.id, requestBody: UpdateArchiveByIdV2025R0RequestBody(name: newArchiveName, description: newArchiveDescription))
+            XCTAssertTrue(updatedArchive.name == newArchiveName)
+            XCTAssertTrue(updatedArchive.description! == newArchiveDescription)
             let archives: ArchivesV2025R0 = try await client.archives.getArchivesV2025R0(queryParams: GetArchivesV2025R0QueryParams(limit: Int64(100)))
             XCTAssertTrue(archives.entries!.count > 0)
             try await client.archives.deleteArchiveByIdV2025R0(archiveId: archive.id)
