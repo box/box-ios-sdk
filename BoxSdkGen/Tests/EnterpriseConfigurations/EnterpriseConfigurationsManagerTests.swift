0fff45ed3139ffebaafc9af4113bbb61d7fd3945
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2025-12-09 11:49:39 +0100

    fix: Remove duplicate optional wrapping (box/box-codegen#898) (#1301)

--- BoxSdkGen/Tests/EnterpriseConfigurations/EnterpriseConfigurationsManagerTests.swift
+++ BoxSdkGen/Tests/EnterpriseConfigurations/EnterpriseConfigurationsManagerTests.swift
@@ -14,15 +14,15 @@ class EnterpriseConfigurationsManagerTests: RetryableTestCase {
             let enterpriseId: String = Utils.getEnvironmentVariable(name: "ENTERPRISE_ID")
             let enterpriseConfiguration: EnterpriseConfigurationV2025R0 = try await adminClient.enterpriseConfigurations.getEnterpriseConfigurationByIdV2025R0(enterpriseId: enterpriseId, queryParams: GetEnterpriseConfigurationByIdV2025R0QueryParams(categories: ["user_settings", "content_and_sharing", "security", "shield"]))
             XCTAssertTrue(Utils.Strings.toString(value: enterpriseConfiguration.type) == "enterprise_configuration")
-            let userSettings: EnterpriseConfigurationUserSettingsV2025R0 = enterpriseConfiguration.userSettings!!
+            let userSettings: EnterpriseConfigurationUserSettingsV2025R0 = enterpriseConfiguration.userSettings!
             XCTAssertTrue(userSettings.isEnterpriseSsoRequired!.value == false)
             XCTAssertTrue(userSettings.newUserDefaultLanguage!.value == "English (US)")
             XCTAssertTrue(userSettings.newUserDefaultStorageLimit!.value == -1)
-            let contentAndSharing: EnterpriseConfigurationContentAndSharingV2025R0 = enterpriseConfiguration.contentAndSharing!!
+            let contentAndSharing: EnterpriseConfigurationContentAndSharingV2025R0 = enterpriseConfiguration.contentAndSharing!
             XCTAssertTrue(contentAndSharing.collaborationPermissions!.value!.isEditorRoleEnabled == true)
-            let security: EnterpriseConfigurationSecurityV2025R0 = enterpriseConfiguration.security!!
+            let security: EnterpriseConfigurationSecurityV2025R0 = enterpriseConfiguration.security!
             XCTAssertTrue(security.isManagedUserSignupEnabled!.value! == false)
-            let shield: EnterpriseConfigurationShieldV2025R0 = enterpriseConfiguration.shield!!
+            let shield: EnterpriseConfigurationShieldV2025R0 = enterpriseConfiguration.shield!
             XCTAssertTrue(shield.shieldRules!.count == 0)
         }
     }
