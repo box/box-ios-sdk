af828ea1d2585b3d117035722028f28a2ad3dcbc
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2025-12-16 16:56:04 +0100

    feat: Treat `nullable` fields as Optional (box/box-codegen#906) (#1325)
    Co-authored-by: box-sdk-build <box-sdk-build@box.com>

--- BoxSdkGen/Sources/Schemas/SignRequestSignerInputCustomValidation/SignRequestSignerInputCustomValidation.swift
+++ BoxSdkGen/Sources/Schemas/SignRequestSignerInputCustomValidation/SignRequestSignerInputCustomValidation.swift
@@ -4,9 +4,9 @@ import Foundation
 /// If set, this validation is mandatory.
 public class SignRequestSignerInputCustomValidation: Codable, RawJSONReadable {
     private enum CodingKeys: String, CodingKey {
+        case validationType = "validation_type"
         case customRegex = "custom_regex"
         case customErrorMessage = "custom_error_message"
-        case validationType = "validation_type"
     }
 
     /// Internal backing store for rawData. Used to store raw dictionary data associated with the instance.
@@ -18,41 +18,41 @@ public class SignRequestSignerInputCustomValidation: Codable, RawJSONReadable {
     }
 
 
-    /// Regular expression used for validation.
-    @CodableTriState public private(set) var customRegex: String
-
-    /// Error message shown if input fails custom regular expression validation.
-    @CodableTriState public private(set) var customErrorMessage: String
-
     /// Defines the validation format for the text input as custom.
     /// A custom regular expression is used for validation.
     public let validationType: SignRequestSignerInputCustomValidationValidationTypeField
 
+    /// Regular expression used for validation.
+    @CodableTriState public private(set) var customRegex: String?
+
+    /// Error message shown if input fails custom regular expression validation.
+    @CodableTriState public private(set) var customErrorMessage: String?
+
     /// Initializer for a SignRequestSignerInputCustomValidation.
     ///
     /// - Parameters:
-    ///   - customRegex: Regular expression used for validation.
-    ///   - customErrorMessage: Error message shown if input fails custom regular expression validation.
     ///   - validationType: Defines the validation format for the text input as custom.
     ///     A custom regular expression is used for validation.
-    public init(customRegex: TriStateField<String>, customErrorMessage: TriStateField<String>, validationType: SignRequestSignerInputCustomValidationValidationTypeField = SignRequestSignerInputCustomValidationValidationTypeField.custom) {
+    ///   - customRegex: Regular expression used for validation.
+    ///   - customErrorMessage: Error message shown if input fails custom regular expression validation.
+    public init(validationType: SignRequestSignerInputCustomValidationValidationTypeField = SignRequestSignerInputCustomValidationValidationTypeField.custom, customRegex: TriStateField<String> = nil, customErrorMessage: TriStateField<String> = nil) {
+        self.validationType = validationType
         self._customRegex = CodableTriState(state: customRegex)
         self._customErrorMessage = CodableTriState(state: customErrorMessage)
-        self.validationType = validationType
     }
 
     required public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
+        validationType = try container.decode(SignRequestSignerInputCustomValidationValidationTypeField.self, forKey: .validationType)
         customRegex = try container.decodeIfPresent(String.self, forKey: .customRegex)
         customErrorMessage = try container.decodeIfPresent(String.self, forKey: .customErrorMessage)
-        validationType = try container.decode(SignRequestSignerInputCustomValidationValidationTypeField.self, forKey: .validationType)
     }
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(validationType, forKey: .validationType)
         try container.encode(field: _customRegex.state, forKey: .customRegex)
         try container.encode(field: _customErrorMessage.state, forKey: .customErrorMessage)
-        try container.encode(validationType, forKey: .validationType)
     }
 
     /// Sets the raw JSON data.
