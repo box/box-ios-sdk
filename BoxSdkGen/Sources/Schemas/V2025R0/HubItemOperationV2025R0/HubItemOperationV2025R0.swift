330e763cc5ca08ab936b20130bda0ea28a8153d2
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2026-03-23 09:52:45 +0100

    feat: Add `reference` to `AI` and `parent_id` to `HubsItems` (box/box-openapi#588) (#1424)

--- BoxSdkGen/Sources/Schemas/V2025R0/HubItemOperationV2025R0/HubItemOperationV2025R0.swift
+++ BoxSdkGen/Sources/Schemas/V2025R0/HubItemOperationV2025R0/HubItemOperationV2025R0.swift
@@ -5,6 +5,7 @@ public class HubItemOperationV2025R0: Codable, RawJSONReadable {
     private enum CodingKeys: String, CodingKey {
         case action
         case item
+        case parentId = "parent_id"
     }
 
     /// Internal backing store for rawData. Used to store raw dictionary data associated with the instance.
@@ -21,26 +22,33 @@ public class HubItemOperationV2025R0: Codable, RawJSONReadable {
 
     public let item: HubItemReferenceV2025R0
 
+    /// The ID of the parent block to add the item to. Must be an Item List block. If not provided, the item will be added to the first page's first Item List block.
+    public let parentId: String?
+
     /// Initializer for a HubItemOperationV2025R0.
     ///
     /// - Parameters:
     ///   - action: The action to perform on a Box Hub item.
     ///   - item: 
-    public init(action: HubItemOperationV2025R0ActionField, item: HubItemReferenceV2025R0) {
+    ///   - parentId: The ID of the parent block to add the item to. Must be an Item List block. If not provided, the item will be added to the first page's first Item List block.
+    public init(action: HubItemOperationV2025R0ActionField, item: HubItemReferenceV2025R0, parentId: String? = nil) {
         self.action = action
         self.item = item
+        self.parentId = parentId
     }
 
     required public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         action = try container.decode(HubItemOperationV2025R0ActionField.self, forKey: .action)
         item = try container.decode(HubItemReferenceV2025R0.self, forKey: .item)
+        parentId = try container.decodeIfPresent(String.self, forKey: .parentId)
     }
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(action, forKey: .action)
         try container.encode(item, forKey: .item)
+        try container.encodeIfPresent(parentId, forKey: .parentId)
     }
 
     /// Sets the raw JSON data.
